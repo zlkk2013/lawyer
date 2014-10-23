757d8374c94e1559b5796ee4ba42fc8982659bdb
gzc <zhecong123@gmail.com>
2014-10-24 01:55:30 +0800

    open activity by name

--- app/src/main/java/buaa/lawyer/MyActivity.java
+++ app/src/main/java/buaa/lawyer/MyActivity.java
@@ -1,17 +1,41 @@
 package buaa.lawyer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+    Button mButton;
+    EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        mButton = (Button)findViewById(R.id.button);
+        mEdit   = (EditText)findViewById(R.id.editText);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String actName = mEdit.getText().toString();
+                try {
+                    Class<Activity> act = (Class<Activity>) Class.forName(actName);
+                    Intent intent = new Intent(MyActivity.this, act);
+                    MyActivity.this.startActivity(intent);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Log.v("EditText", actName);
+            }
+        });
     }
 
 
